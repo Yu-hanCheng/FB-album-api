ad555fc13c833169a5233cb23588418b68fa0314
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-02-04 13:44:17 +0800

    debug wrong comment out

--- app/Http/Controllers/CrawlerController.php
+++ app/Http/Controllers/CrawlerController.php
@@ -19,34 +19,34 @@ class CrawlerController extends Controller
         $album_id= $request->album_id;
         $client = new Client();
         try {
+            // 取得相簿中的所有相片
             $response_origin = $client->request('GET', env('FB_BASE_URL').$album_id.'/photos?access_token='.env('ACCESS_TOKEN'));
         } catch (\Throwable $th) {
             return response()->json(['result'=>$th],500);
         }     
-        //$response
+        
         $response = json_decode($response_origin->getBody());
         $out = new \Symfony\Component\Console\Output\ConsoleOutput();
         $next="";
-        $download = false;
         do {
             $next=false;
             foreach ($response->data as $menu){
             
                 $output = preg_split( "/[\s\n,]+/", $menu->name );
-                // try {
-                //     mkdir($output[0]);
-                // } catch (\Throwable $th){
-                //     continue;
-                // }
+                // 取出描述的第一段(即店家名稱) 當資料夾名稱
+                try {
+                    mkdir($output[0]);
+                } catch (\Throwable $th){
+                    continue;
+                }
+                // 下載相簿中的相片(即菜單)
+                $menu_img_origin = $client->request('GET', env('FB_BASE_URL').$menu->id.'?fields=images&access_token='.env('ACCESS_TOKEN'));
+                $menu_img = json_decode($menu_img_origin->getBody())->images;
+                $the_img = $menu_img[0]->source;
+                $imgName = $output[0].'/menu.jpg';
+                file_put_contents($imgName, file_get_contents($the_img));
                 
-                
-                // $menu_img_origin = $client->request('GET', env('FB_BASE_URL').$menu->id.'?fields=images&access_token='.env('ACCESS_TOKEN'));
-                // $menu_img = json_decode($menu_img_origin->getBody())->images;
-                // $the_img = $menu_img[0]->source;
-                // $imgName = $output[0].'/menu.jpg';
-                // file_put_contents($imgName, file_get_contents($the_img));
-                
-                
+                // 取得該菜單的所有留言
                 $all_comments_origin = $client->request('GET', env('FB_BASE_URL').$menu->id.'?fields=comments&access_token='.env('ACCESS_TOKEN'));
                 $all_comments = json_decode($all_comments_origin->getBody());
                 
@@ -55,28 +55,17 @@ class CrawlerController extends Controller
                 }
                 $all_comments_data = $all_comments->comments->data;
                 
-                foreach (array_slice($all_comments_data,1) as $comment){
-                    // $souceImg=$comment->message."   ".$comment->id;
-                                     
-                    if (!$download) {
-                        if ($comment->id !== "1899205933474779_1899283883466984") {
-                            continue;
-                        } else{
-                            $out->writeln("start download".$output[0]);
-                            $download=true;
-                        } 
-                    } else{
-                        $comment_attachment_origin = $client->request('GET', env('FB_BASE_URL').$comment->id.'/?fields=attachment&access_token='.env('ACCESS_TOKEN'));
-                        $comment_attachment = json_decode($comment_attachment_origin->getBody());
-                        try {
-                            $souceImg=$comment_attachment->attachment->media->image->src;
-                            $imgName = $output[0].'/'.$comment->message.'.jpg';
-                            file_put_contents($imgName, file_get_contents($souceImg));
-                        } catch (\Throwable $th) {
-                            continue;
-                        }
+                foreach ($all_comments_data as $comment){
+                    // 取得該菜單留言中的附件(即菜色照片)
+                    $comment_attachment_origin = $client->request('GET', env('FB_BASE_URL').$comment->id.'/?fields=attachment&access_token='.env('ACCESS_TOKEN'));
+                    $comment_attachment = json_decode($comment_attachment_origin->getBody());
+                    try {
+                        $souceImg=$comment_attachment->attachment->media->image->src;
+                        $imgName = $output[0].'/'.$comment->message.'.jpg';
+                        file_put_contents($imgName, file_get_contents($souceImg));
+                    } catch (\Throwable $th) {
+                        continue;
                     }
-                    
                 }
             
             }
@@ -96,9 +85,6 @@ class CrawlerController extends Controller
             }
         } while ($next);
     
-        
-        // dd($response);
-        // file_put_contents($imgName, file_get_contents($souceImg));
-        return response()->json(['result'=>$response],200);
+        return response()->json(['result'=>"done"],200);
     }
 }
